0fb650cf5c190e4f63c4a1f93956c4f99ee4a0b1
ming <2822564634@qq.com>
2023-06-08 14:39:55 +0800

    finish accounts

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -11,18 +11,24 @@ const db = low(adapter)
 
 //记账本列表
 router.get('/record', function(req, res, next) {
-  res.render('record');
+  res.render('record')
 });
 
-router.get('/table', function(req, res, next) {
-  res.render('table');
-});
+// router.get('/table', function(req, res, next) {
+//   res.render('table')
+// });
+
+router.get('/accounts',(req,res)=>{
+  let accounts = db.get('accounts').value()
+  console.log(accounts)
+  res.render('accounts',{accounts:accounts})
+})
 
 router.post('/record',(req,res)=>{
   let id = shortid.generate()
   // console.log(req.body);
   db.get('accounts').unshift({id:id,...req.body}).write()
-  res.render('tip',({msg:'添加事项成功',url:'/accounts/record'}))
+  res.render('tip',({msg:'添加事项成功',url:'/accounts/accounts'}))
 })
 
 module.exports = router;
